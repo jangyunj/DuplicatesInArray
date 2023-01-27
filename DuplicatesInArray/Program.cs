16faa933196c9f29472f4231a863b759452b2721
Yun Jang <jang.yunj7@gmail.com>
2023-01-26 20:15:15 -0500

    initial commit

---
+++ DuplicatesInArray/Program.cs
@@ -0,0 +1,22 @@
+﻿//Define duplicates
+int[] array = new int[] { 1, 2, 3, 4, 5, 6, 1, 7, 8, 9 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[i] == array[j])
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
+
+
+//Output without duplicate value
+var array1 = new int[] { 2, 3, 3, 4, 1, 2, 5 };
+var distinct1 = array1.Distinct().ToArray();
+
+Console.WriteLine(String.Join(",", distinct1));
+
+
